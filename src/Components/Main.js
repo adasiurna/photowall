ae605774cea059d598562290e20d904a527002c9
Ada Šiurna <38506287+adasiurna@users.noreply.github.com>
2020-08-25 21:29:30 +0300

    reducer.js still does not get that it has posts

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -16,7 +16,7 @@ class Main extends Component {
   }
 
   render() {
-    console.log(this.props);
+    //console.log(this.props);
     return (
       <div>
         <Route exact path='/' render={() => (
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -9,9 +9,10 @@ function PhotoWall(props) { //kadangi tai stateless function, nereikia naudoti t
   return <div>
     <Link className="addIcon" to="/AddPhoto"></Link>
     <div className="photoGrid">
-      {props.posts.sort(function (x, y) {
-        return y.id - x.id
-      })
+      {props.posts
+        .sort(function (x, y) {
+          return y.id - x.id
+        })
         .map((post, index) => <Photo key={index} post={post} {...props} index={index} />)}
     </div>
   </div>
